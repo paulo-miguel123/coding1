43787530da1e059e34db4f23b89250c1f1b71b94
paulo-miguel123 <ppedroca20088@gmail.com>
2024-09-25 10:39:50 -0300

    criei o repositorio

---
+++ aula.js
@@ -0,0 +1,47 @@
+/*npm init -y
+//npm install node
+
+console.log("hello word");
+
+var rotulo = "natalya"
+console.log(rotulo)
+
+rotulo = 12345
+console.log(rotulo)
+
+var soma = 10 + 25
+console.log(soma)
+
+rotulo = "batata"
+console.log(rotulo)
+
+console.log('hello'+' '+'world')
+
+console.log('hello'+'world')
+
+myName = "paulo";
+console.log("hello"+' '+myName )
+
+myName2 = "miguel"
+console.log(`hello ${myName2}`)
+
+var soma = 10 + '25'
+console.log(soma)
+
+myName3 = "3";
+console.log(`hello${myName3}`)
+
+console.log(2 == 2)
+
+console.log(2 == "casa")
+
+console.log(2 == "2")
+
+console.log("house" === "casa")
+
+console.log("house" === "house")
+
+console.log(2<20)
+console.log(89<7)
+
+console.log(2 + 2 ==4)*/
